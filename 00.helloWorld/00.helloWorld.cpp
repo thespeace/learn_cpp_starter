554ea45e591151037c839466e46be1b3e1980f9d
thespeace <thespeace1@gmail.com>
2024-03-01 22:11:34 +0900

    Initial commit

---
+++ 00.helloWorld/00.helloWorld.cpp
@@ -0,0 +1,10 @@
+﻿// `#` : 전처리(소스코드가 컴파일되기 이전 단계) 지시자
+
+#include <iostream>
+
+int main() //메인 함수, Entry point of the program
+{
+    std::cout << "Hello World!\n";
+	return 0; //exit code, 생략 가능(default:0)
+}
+// 0=main()
